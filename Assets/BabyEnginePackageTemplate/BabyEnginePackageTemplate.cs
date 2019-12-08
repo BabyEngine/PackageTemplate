707ed427ee4d1a46771205f236efab6241d19b9c
ade <ade.li.indie@gmail.com>
2019-12-08 09:04:04 +0800

    init

---
+++ Assets/BabyEnginePackageTemplate/BabyEnginePackageTemplate.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BybyEngine {
+    public static class BabyEnginePackageTemplate {
+        public static string Test() {
+            return "Yes!BabyEnginePackageTemplate";
+        }
+    }
+}
